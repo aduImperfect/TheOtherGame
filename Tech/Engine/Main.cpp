211bde7e5d02634aa12317797665420e36247991
DESKTOP-NAD15B0\Aditya PC <adu101992>
2017-10-14 13:22:08 +0530

    - Added Empty Project Solution Tech. - Skeleton folders added for Engine, Libraries, Assets, Configuration - Added Main.cpp - Added Precompiled.h,.cpp which includes all the STL and other included libraries for the project. Can add generic constants and type definitions which will be used globally throughout the project here. - Added SOIL library to the project (Example - can be changed as needed). - Added filters for Engine code sections: Audio, Core, Gameplay, Graphics, Input, Physics, Serialization, UI, Utility. Will add further folders upon this folder structure being organized. - Added Documents folder to the main git folder which can contain any Documents regarding the game.

---
+++ Tech/Engine/Main.cpp
@@ -0,0 +1,6 @@
+#include "Precompiled.h"
+
+int main()
+{
+	return 0;
+}
---
+++ Tech/Engine/Precompiled.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+//Standard Template Libraries
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <string>
+#include <list>
+#include <map>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+
+//Libraries included
+#include "SOIL.h"
+
